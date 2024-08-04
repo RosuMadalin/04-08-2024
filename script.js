6be35d2408bbb8b5b9ea537909d6997d0a43d951
RosuMadalin <155835861+RosuMadalin@users.noreply.github.com>
2024-08-04 14:04:32 +0300

    adaugare ora locala

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const dateContainer = document.getElementById('date-container');
-    const currentDate = new Date();
+
+    const madalinFunction = () => {
+
+        /// continut start
+        const dateContainer = document.getElementById('date-container');
+        const currentDate = new Date();
+        
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedDate = currentDate.toLocaleDateString(undefined, options);
+        
+        
+        const formattedTime = String(currentDate.getHours()).padStart(2, '0') + ":"  
+                    + String(currentDate.getMinutes()).padStart(2, '0') + ":" 
+                    + String(currentDate.getSeconds()).padStart(2, '0');
+        
+        dateContainer.textContent = `Today's date is: ${formattedDate}, and time is: ${formattedTime}`;
+        // continut sfarsit
+
+    };
     
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = currentDate.toLocaleDateString(undefined, options);
-    
-    dateContainer.textContent = `Today's date is: ${formattedDate}`;
+    madalinFunction();
+
+    // Update time every second
+    setInterval(madalinFunction, 1000);
 });
